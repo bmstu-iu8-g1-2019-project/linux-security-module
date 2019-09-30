9870199902da3d03ed7311136a83665da18b8068
fktrcfylh1234567 <fktrcfylh1234567@yandex.ru>
2019-09-30 08:35:10 +0300

    move logic from hooks

--- module.c
+++ module.c
@@ -1,246 +1,206 @@
-// /security/bmstu/module.c
-//---INCLUDES
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/kernel.h>
-#include <linux/ptrace.h>
-#include <linux/errno.h>
-#include <linux/sched.h>
-#include <linux/security.h>
-#include <linux/xattr.h>
-#include <linux/capability.h>
-#include <linux/unistd.h>
-#include <linux/mm.h>
-#include <linux/mman.h>
-#include <linux/slab.h>
-#include <linux/pagemap.h>
-#include <linux/swap.h>
-#include <linux/spinlock.h>
-#include <linux/syscalls.h>
-#include <linux/file.h>
-#include <linux/namei.h>
-#include <linux/mount.h>
-#include <linux/ext2_fs.h>
-#include <linux/proc_fs.h>
-#include <linux/fs.h>
-#include <linux/kd.h>
-#include <linux/stat.h>
-#include <asm/uaccess.h>
-#include <asm/ioctls.h>
-#include <linux/bitops.h>
-#include <linux/interrupt.h>
-#include <linux/quota.h>
-#include <linux/parser.h>
-#include <linux/nfs_mount.h>
-#include <linux/hugetlb.h>
-#include <linux/personality.h>
-#include <linux/sysctl.h>
-#include <linux/audit.h>
-#include <linux/string.h>
-#include <linux/uidgid.h>
 #include <linux/lsm_hooks.h>
 #include <linux/usb.h>
+#include <linux/xattr.h>
+#include <linux/syscalls.h>
+#include <linux/fs.h>
 
 MODULE_AUTHOR("fktrc");
 MODULE_DESCRIPTION("BMSTU Linux Security Module");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("0.0.1");
 
-
-static inline bool is_root_uid(void) {
+inline bool is_root_uid(void) {
+	const struct cred *cred;
+	cred = current_cred();
 	return uid_eq(current_uid(), GLOBAL_ROOT_UID);
 }
 
-static int match_device(struct usb_device *dev, void *p)
+int match_device(struct usb_device *dev, void *p)
 {
-    char *product = dev->product;
-    char *manufacturer = dev->manufacturer;
-    char *serial = dev->serial;
-    
+    char *product;
+    char *manufacturer;
+    char *serial;
+
+    product = dev->product;
+    manufacturer = dev->manufacturer;
+    serial = dev->serial;
+
 	printk("bmstuLogs usb_device product %s, serial %s\n", product, serial);
 	if (strcmp(serial, "9HHORL8W") == 0)
 	{
 		return 1;
 	}
-	
+
 	return 0;
 }
 
-static int find_usb_device(void)
+int find_usb_device(void)
 {
-	void *p;
-	int match = usb_for_each_dev(p, match_device);
+	void *p = NULL;
+	bool match = usb_for_each_dev(p, match_device);
     return match;
+}
+
+char* get_file_xattr(struct file *file)
+{
+	char *buff = kcalloc(32, sizeof(char), GFP_KERNEL);
+	int size_buff = 32*sizeof(char);
+	int res;
+
+	if (buff == NULL)
+	{
+		return NULL;
+	}
+
+	res = vfs_getxattr(file->f_path.dentry, "user.bmstu", buff, size_buff);
+	return buff;
+}
+
+bool inode_is_dir(struct inode *inode)
+{
+	return (inode->i_mode & S_IFMT) == S_IFDIR;
+}
+
+int file_may_access(struct file *file)
+{
+	char *path;
+	char *attr;
+    char *current_dentry;
+    char *parent_dentry;
+    char buff[256];
+
+	if (is_root_uid())
+	{
+		return 0;
+	}
+
+	if (!file)
+	{
+		return 0;
+	}
+
+    current_dentry = file->f_path.dentry->d_iname;
+    parent_dentry = file->f_path.dentry->d_parent->d_iname;
+    path = dentry_path_raw(file->f_path.dentry, buff, 256);
+
+	if(strstr(path, "/home/") == NULL)
+	{
+		return 0;
+	}
+
+	attr = get_file_xattr(file);
+
+	if (strcmp(attr, "bruh") != 0)
+	{
+		printk("bmstuLogs non-protected file access %s\n", path);
+        return 0;
+	}
+
+	if (find_usb_device())
+	{
+		printk("bmstuLogs USB device found. Access granted! %s\n", path);
+        return 0;
+	}
+
+    printk("bmstuLogs You shall not pass!\n");
+    return -EACCES;
+}
+
+int inode_may_access(struct inode *inode)
+{
+	struct dentry *dentry;
+    char *path = NULL;
+    char buf[64];
+
+	if (is_root_uid())
+	{
+		return 0;
+	}
+
+	if (!inode)
+	{
+		return 0;
+	}
+
+	spin_lock(&inode->i_lock);
+	hlist_for_each_entry(dentry, &inode->i_dentry, d_u.d_alias) {
+    	path = dentry_path_raw(dentry, buf, sizeof(buf));
+    }
+	spin_unlock(&inode->i_lock);
+
+	if (path == NULL)
+	{
+		return 0;
+	}
+
+	if (strstr(path, "/home/") == NULL)
+	{
+		return 0;
+	}
+
+	printk("bmstuLogs inode_may_access %s\n", path);
+
+	return 0;
 }
 
 //---HOOKS
 
-static int bmstu_inode_permission(struct inode *inode, int mask)
-{		
-	if (is_root_uid())
-	{
-		return 0;
-	}
-
-	if ((inode->i_mode & S_IFMT) == S_IFDIR) {
-		return 0;
-	}
-		
-	const struct cred *cred = current_cred();	
-	struct dentry *dentry;
-	char buf[64];
-    const char *path;
-	
-	spin_lock(&inode->i_lock);
-	hlist_for_each_entry(dentry, &inode->i_dentry, d_u.d_alias) {
-    	path = dentry_path_raw(dentry, buf, sizeof(buf));
-		if(strstr(path, "/home/") != NULL)
-		{
-			printk("bmstuLogs inode_permission path %s\n", path);
-		}
-    }
-	spin_unlock(&inode->i_lock);	
-		
-	return 0;
+static int bmstu_file_open(struct file *file)
+{
+	return file_may_access(file);
 }
 
 static int bmstu_file_permission(struct file *file, int mask)
 {
-	if (is_root_uid())
-	{
-		return 0;
-	}
-	
-	if (!file)
-	{
-		return 0;
-	}
-	
-	char *path;
-    char *dentry;
-    char *parent_dentry;
-    char buff[256];
-    
-    dentry = file->f_path.dentry->d_iname;
-    parent_dentry = file->f_path.dentry->d_parent->d_iname;
-    path = dentry_path_raw(file->f_path.dentry, buff, 256);
-
-	if(strstr(path, "/home/") == NULL)
-	{
-		return 0;
-	}
-	
-	printk("bmstuLogs file_permission hook at %s, mask %i\n", path, mask);
-	return 0;
+	return file_may_access(file);
 }
 
-static int bmstu_file_open(struct file *file)
-{    
-	if (is_root_uid())
-	{
-		return 0;
-	}
-	
-	if (!file)
-	{
-		return 0;
-	}
-	
-	char *path;
-    char *current_dentry;
-    char *parent_dentry;
-    char buff[256];
-    
-    current_dentry = file->f_path.dentry->d_iname;
-    parent_dentry = file->f_path.dentry->d_parent->d_iname;
-    path = dentry_path_raw(file->f_path.dentry, buff, 256);
-    
-	if(strstr(path, "/home/") == NULL)
-	{
-		return 0;
-	}
-
-	char buf[1024];
-	int size_buf = 1024;
-	int res;
-
-	res = vfs_getxattr(file->f_path.dentry, "user.bmstu", buf, size_buf);
-	
-	if (strcmp(buf, "bruh") != 0)
-	{
-		printk("bmstuLogs file open at %s\n", path);
-        return 0;
-	}
-	
-	if (find_usb_device())
-	{
-		printk("bmstuLogs file open at %s\n", path);
-		printk("bmstuLogs USB device found. Access granted!\n");
-        return 0;
-	}
-	
-    printk("file open: You shall not pass!\n");
-    return -EACCES;
-}					   
+static int bmstu_inode_permission(struct inode *inode, int mask)
+{
+	return inode_may_access(inode);
+}
 
 static int bmstu_inode_setxattr(struct dentry *dentry, const char *name,
 				  const void *value, size_t size, int flags)
 {
+	struct inode *inode;
+
 	if (is_root_uid())
 	{
 		return 0;
 	}
 
-	struct inode *inode = d_backing_inode(dentry);
-	printk("bmstuLogs bmstu_inode_setxattr hook %s\n", name);
+	inode = d_backing_inode(dentry);
+
 	return 0;
 }
 
 static int bmstu_inode_getxattr(struct dentry *dentry, const char *name)
 {
-	if (is_root_uid())
-	{
-		return 0;
-	}
-
-	printk("bmstuLogs bmstu_inode_getxattr hook %s\n", name);
 	return 0;
 }
 
 static int bmstu_inode_listxattr(struct dentry *dentry)
 {
-	if (is_root_uid())
-	{
-		return 0;
-	}
-
-	const struct cred *cred = current_cred();
-	printk("bmstuLogs bmstu_inode_listxattr hook\n");
 	return 0;
 }
 
 static int bmstu_inode_removexattr(struct dentry *dentry, const char *name)
 {
-	if (is_root_uid())
-	{
-		return 0;
-	}
-
-	printk("bmstuLogs bmstu_inode_removexattr hook %s\n", name);
 	return 0;
 }
 
 //---HOOKS REGISTERING
 static struct security_hook_list bmstu_hooks[] =
 {
-	//LSM_HOOK_INIT(inode_permission, bmstu_inode_permission),
-	//LSM_HOOK_INIT(file_permission, bmstu_file_permission),
 	LSM_HOOK_INIT(file_open, bmstu_file_open),
-	
-	//LSM_HOOK_INIT(inode_setxattr, bmstu_inode_setxattr),
-	//LSM_HOOK_INIT(inode_getxattr, bmstu_inode_getxattr),
-	//LSM_HOOK_INIT(inode_listxattr, bmstu_inode_listxattr),
-	//LSM_HOOK_INIT(inode_removexattr, bmstu_inode_removexattr),
+	LSM_HOOK_INIT(file_permission, bmstu_file_permission),
+
+	LSM_HOOK_INIT(inode_permission, bmstu_inode_permission),
+	LSM_HOOK_INIT(inode_setxattr, bmstu_inode_setxattr),
+	LSM_HOOK_INIT(inode_getxattr, bmstu_inode_getxattr),
+	LSM_HOOK_INIT(inode_listxattr, bmstu_inode_listxattr),
+	LSM_HOOK_INIT(inode_removexattr, bmstu_inode_removexattr),
 };
 
 //---INIT
